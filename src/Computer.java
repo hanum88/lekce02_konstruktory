17e18bd3397e06789dfda41983a8278962dd74d0
hanum88 <hanahrbacova04@gmail.com>
2025-04-24 20:12:31 +0200

    konstruktory

--- src/Computer.java
+++ src/Computer.java
@@ -1,4 +1,71 @@
-package PACKAGE_NAME;
+import java.math.BigDecimal;
 
 public class Computer {
+    private String model;
+    private double diagonalOfScreen;
+    private boolean isCompanyComputer;
+    private BigDecimal price;
+    private boolean isLaptop;
+
+    public boolean isOverPriceLimit (BigDecimal priceLimit) {
+        return price.compareTo(priceLimit) > 0;
+    }
+
+
+    public boolean getIsLaptop() {
+        return isLaptop;
+    }
+
+    public void setIsLaptop(boolean type) {
+        this.isLaptop = isLaptop;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public boolean isCompanyComputer() {
+        return isCompanyComputer;
+    }
+
+    public void setCompanyComputer(boolean companyComputer) {
+        isCompanyComputer = companyComputer;
+    }
+
+    public double getDiagonalOfScreen() {
+        return diagonalOfScreen;
+    }
+
+    public void setDiagonalOfScreen(double diagonalOfScreen) {
+        this.diagonalOfScreen = diagonalOfScreen;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+   public Computer(String model, double diagonalOfScreen, boolean isCompanyComputer, BigDecimal price, boolean isLaptop) {
+        this.model = model;
+        this.diagonalOfScreen = diagonalOfScreen;
+        this.isCompanyComputer = isCompanyComputer;
+        this.price = price;
+        this.isLaptop = isLaptop;
+    }
+    public Computer (String model, BigDecimal price){
+       /* this.model = "DEF";
+        this.diagonalOfScreen = 25;
+        this.isCompanyComputer = true;
+        this.price = price;
+        this.isLaptop = true; nahrazeno this+konstruktor*/
+        this(model, 15.6, true, price, true);
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Computer computer = new Computer("macbook", 15.5, true, new BigDecimal(2000), false);
+        computer.setModel("ABC");
+        System.out.println(computer.getModel());
+
+             System.out.println("cena je: " + computer.getPrice());
+
+       System.out.println("je překročen cenový limit? " +  computer.isOverPriceLimit(new BigDecimal(2000000)));
+
+       Computer secondComputer = new Computer("nejnovější model", new BigDecimal(25000));
+        System.out.println("model second computeru: " + secondComputer.getModel());
+        System.out.println("cena second computeru: " + secondComputer.getPrice());
+
     }
 }
